d3b316c91ed50c270b3ced8f720fafca22c21bf1
fanoplanes <pontryagin@protonmail.com>
2024-12-01 13:46:22 +0100

    lint

--- 01/01-2.c
+++ 01/01-2.c
@@ -1,49 +1,51 @@
 #include <stdio.h>
 
-int compare_ints(const void* a, const void* b) {
-    int arg1 = *(const int*)a;
-    int arg2 = *(const int*)b;
+int compare_ints(const void* a, const void* b)
+{
+	int arg1 = *(const int*)a;
+	int arg2 = *(const int*)b;
 
-    if (arg1 < arg2) return -1;
-    if (arg1 > arg2) return 1;
-    return 0;
+	if (arg1 < arg2) return -1;
+	if (arg1 > arg2) return 1;
+	return 0;
 }
 
-int main(void) {
-    FILE *fp = fopen("input", "r");
-    if(!fp) {
-        printf("YIKES!");
-        return -1;
-    }
+int main(void)
+{
+	FILE *fp = fopen("input", "r");
+	if(!fp) {
+		printf("YIKES!");
+		return -1;
+	}
 
-    char cr;
-    size_t lines = 0;
+	char cr;
+	size_t lines = 0;
 
-    while(cr != EOF) {
-        if(cr == '\n') {
-            lines++;
-        }
-        cr = getc(fp);
-    }
-    rewind(fp);
+	while(cr != EOF) {
+		if(cr == '\n') {
+			lines++;
+		}
+		cr = getc(fp);
+	}
+	rewind(fp);
 
-    int fst[lines];
-    int snd[lines];
+	int fst[lines];
+	int snd[lines];
 
-    for(int i = 0; i < lines; i++) {
-        fscanf(fp, "%i   %i\n", &fst[i], &snd[i]);
-    }
-    fclose(fp);
+	for(int i = 0; i < lines; i++) {
+		fscanf(fp, "%i   %i\n", &fst[i], &snd[i]);
+	}
+	fclose(fp);
 
-    int answer = 0;
+	int answer = 0;
 
-    for(int i = 0; i < lines; i++) {
-        for(int j = 0; j < lines; j++) {
-            if (fst[i] == snd[j]) answer += fst[i];
-        }
-    }
+	for(int i = 0; i < lines; i++) {
+		for(int j = 0; j < lines; j++) {
+			if (fst[i] == snd[j]) answer += fst[i];
+		}
+	}
 
-    printf("%i\n", answer);
+	printf("%i\n", answer);
 
-    return 0;
-    }
+	return 0;
+}
