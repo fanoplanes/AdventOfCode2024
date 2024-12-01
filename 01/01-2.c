175eb7e63d75b5d16eef6488e2534192900109e8
fanoplanes <pontryagin@protonmail.com>
2024-12-01 17:20:47 +0100

    unfuckening 2: electric boogaloo

--- 01/01-2.c
+++ 01/01-2.c
@@ -1,15 +1,5 @@
 #include <stdio.h>
 
-int compare_ints(const void* a, const void* b)
-{
-	int arg1 = *(const int*)a;
-	int arg2 = *(const int*)b;
-
-	if (arg1 < arg2) return -1;
-	if (arg1 > arg2) return 1;
-	return 0;
-}
-
 int main(void)
 {
 	FILE *fp = fopen("input", "r");
