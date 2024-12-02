efc95c3b3028febf675cc64bf36e46de9e52291b
fanoplanes <pontryagin@protonmail.com>
2024-12-02 18:03:46 +0100

    Added day 2, this time only in racket

---
+++ 02/02-2.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+(define input (file->lines "input"))
+
+(define separated (map string-split input))
+
+(define numberified
+  (for/list ([i separated])
+    (map string->number i)))
+
+(define (diff line)
+  (for/list ([i (sub1 (length line))])
+    (- (list-ref line (+ 1 i)) (list-ref line i))))
+
+(define (change-ok? i)
+  (cond
+    [(> i 3) false]
+    [(< i -3) false]
+    [(= i 0) false]
+    [else true]))
+
+(define (check-change line)
+  (for/and ([i (diff line)])
+    (change-ok? i)))
+
+(define (check-order line)
+  (cond
+    [(apply > line) true]
+    [(apply < line) true]
+    [else false]))
+
+(define (process line)
+  (and (check-order line) (check-change line)))
+
+(define (random-bullshit-go line)
+  (if (ormap process (combinations line (sub1 (length line)))) 1 0))
+
+(define answer (for/sum ([i numberified])
+  (random-bullshit-go i)))
+
+answer
