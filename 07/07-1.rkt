280dd4b5f6a7f1309fdd74a940fe9b2de228754c
fanoplanes <pontryagin@protonmail.com>
2024-12-07 12:50:43 +0100

    Added day 7.

---
+++ 07/07-1.rkt
@@ -0,0 +1,20 @@
+#lang racket
+
+(define input (map (lambda (line) (string-split line ":")) (file->lines "input")))
+(define target (map (lambda (line) (string->number (car line))) input))
+(define elements (map (lambda (line) (map string->number (string-split (cadr line)))) input))
+
+(define (apply-proc proc lst)
+  (cons (apply proc (take lst 2)) (drop lst 2)))
+
+(define (process-line line)
+  (flatten (for/list ([i (in-list (list * +))]
+                      #:final (= (length line) 1))
+             (if (= (length line) 1)
+                 line
+                 (process-line (apply-proc i line))))))
+
+(define (can-be? target line)
+  (if (member target (process-line line)) #t #f))
+
+(for/sum ([i target] [j elements] #:when (can-be? i j)) i)
---
+++ 07/07-2.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define input (map (lambda (line) (string-split line ":")) (file->lines "input")))
+(define target (map (lambda (line) (string->number (car line))) input))
+(define elements (map (lambda (line) (map string->number (string-split (cadr line)))) input))
+
+(define (apply-proc proc lst)
+  (cons (apply proc (take lst 2)) (drop lst 2)))
+
+(define (concat x y)
+  (string->number (string-append (number->string x) (number->string y))))
+
+(define (process-line line)
+  (flatten (for/list ([i (in-list (list * + concat))]
+                      #:final (= (length line) 1))
+             (if (= (length line) 1)
+                 line
+                 (process-line (apply-proc i line))))))
+
+(define (can-be? target line)
+  (if (member target (process-line line)) #t #f))
+
+(for/sum ([i target] [j elements] #:when (can-be? i j)) i)
